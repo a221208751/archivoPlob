c6aed97e4b686ef1d2a7e2c3227d0a9882d098c6
Jesus Obregon <a221208751@unison.mx>
2022-09-27 16:52:35 -0700

    PracticaListasObregon

--- hilos/src/PracticHilos.java
+++ hilos/src/PracticHilos.java
@@ -4,9 +4,13 @@ public class PracticHilos implements Runnable {
     @Override
     public void run() {
         int fact = 1;
-        for(int i = nums; i > 0; i--){
-            fact = fact * i;
+        if(nums == 0){
+            System.out.println("Factorial de 0 es igual a: 1");
+        } else {
+            for(int i = nums; i > 0; i--){
+                fact = fact * i;
+            }
+            System.out.println(Thread.currentThread().getName() + ":" + fact);
         }
-        System.out.println(Thread.currentThread().getName() + ":" + fact);
     }
 }
