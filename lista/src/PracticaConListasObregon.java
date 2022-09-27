969bdf6e3af34d6ee9da62b168406c9ec7264a3f
Jesus Obregon <a221208751@unison.mx>
2022-09-27 16:56:07 -0700

    PracticaListasObregon

--- lista/src/PracticaConListasObregon.java
+++ lista/src/PracticaConListasObregon.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-
+//jesus
 
 public class PracticaConListasObregon {
     public static void main(String[] args) {
+
         archivo a = new archivo();
         String divinaComedia = a.leerTXT("C:\\Users\\jesus\\Downloads\\txt\\divina_comedia.txt");
         String [] palabras = divinaComedia.split(" ");
         System.out.println("Lista del total de palabras:");
         for(int i = 0; i < palabras.length; i++){
-            System.out.println(i+".-" + palabras[i]);
+            System.out.println(i+".- " + palabras[i]);
         }
         System.out.println("Ingrese la palabra a buscar:");
         Scanner sc = new Scanner(System.in);
